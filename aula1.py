ac9da2ca5a20ea4dad77f0cf1c3c783bbd40bf95
jub1212 <jub191778@gmail.com>
2026-02-11 12:41:21 -0300

    mensagem

---
+++ aula1.py
@@ -0,0 +1,88 @@
+# operadores aritmetricos
+# entrada e escrita de dados
+# estrutura de decisao e logica condicional
+
+# entrada e escrita
+
+# nome = input("digite seu nome: ")
+# idade = input("digite sua idade: ")
+
+# print("o nome e: ", nome, "\n idade e de:", idade)
+
+#fazer um programa que ira perguntar o nome, a profissao e quanto ele recebe e mostrar os dados digitados
+
+# nome = input("nome: ")
+# idade = input("idade: ")
+# profissao = input("profissao: ")
+# quanto = input("quanto recebe: ")
+
+# print("o nome e: ", nome, "\n a idade e: ", idade, "\n a profissao e: ", profissao, "\n voce recebe: ", quanto)
+
+
+
+# int e para numeros inteiros
+# int  sao numeros que nao possuem casas decimais
+# float e para numeros decimais
+# float aceita numeros quebrados
+
+#cast
+
+#cast
+# valor1 = int(input("digite o valor 1: "))
+# valor2 = int(input("digite o valor 2: "))
+
+# soma = valor1 + valor2
+
+# print("a soma dos valores e de:", soma)
+# valor2 = int(input("digite o valor 2: "))
+
+# # soma = +, multiplicacao = *, divisao = /, subtracao = -
+
+# soma = valor1 + valor2
+# multplicacao = valor1 * valor2
+
+
+# print("a soma dos valores e de:", soma)
+
+
+
+# cast para float
+# valor1 = float(input("digite o valor 1: "))
+
+# soma = 1.5 + valor1
+
+# print("soma e de", soma)
+
+
+# peso = float(input("digite o seu peso em kg: "))
+# altura = float(input("digite sua altura em metros: "))
+
+
+# imc = peso / (altura * altura)
+
+# print("o imc e de", imc)
+
+
+a = float(input("digite o valor a: "))
+b = float(input("digite o valor b: "))
+c = float(input("digite o valor c: "))
+
+delta = (b**2) - (4 * a * c)
+
+
+x1 = (-b + (delta**0.5)) / 2 * a
+
+x2 = (-b - (delta**0.5)) / 2 * a
+
+print("x1 =", x1, "x2 =", x2)
+
+
+
+
+
+
+
+
+
+
+
